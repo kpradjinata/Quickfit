086b8254524cdc833c27caf4559f5363edf83e1b
kpradjinata <kpradjinata@gmail.com>
2021-03-20 01:12:35 -0700

    did nothing

---
+++ Workouts/New Group/InfoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InfoViewController.swift
+//  Workouts
+//
+//  Created by Kevin Pradjinata on 3/20/21.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController!.navigationBar.isTranslucent = false
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
